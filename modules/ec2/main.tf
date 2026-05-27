ccfd297864812e5aac6416434db06daa72d3e69e
Eduardito Urbina <eduardourbin@gmail.com>
2026-05-27 16:32:28 -0400

    docs: implementar tipado estricto y descripciones detalladas en variables (IL3.2)

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,10 +1,16 @@
+locals {
+  user_data_script = var.user_data != null ? var.user_data : file(
+    "${path.module}/templates/user_data.sh"
+  )
+}
+
 resource "aws_instance" "this" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
   vpc_security_group_ids = [aws_security_group.this.id]
 
-  user_data = var.user_data
+  user_data = local.user_data_script
 
   tags = merge(
     {
